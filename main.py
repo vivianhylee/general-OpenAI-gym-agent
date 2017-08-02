44cadcb7c368ca97fec306c06e7db75f3832e437
vivianhylee <vivianhylee@alumni.stanford.edu>
2017-08-01 19:33:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,19 @@ def train(environment, model_name=None, key=None):
     tdir = tempfile.mkdtemp()
     env = gym.make(environment)
     env = gym.wrappers.Monitor(env, tdir, force=True)
-
     agent = DQNAgent(env)
     EPISODES = 600
-
     for episode in range(EPISODES):
         state, reward, done = env.reset(), 0.0, False
         action = agent.action(state, reward, done, episode)
-
         while not done:
             # env.render()
             next_state, reward, done, _ = env.step(action)
             agent.store(state, action, reward, next_state, done)
             state = next_state
             action = agent.action(state, reward, done, episode)
-
         if model_name and episode % 50 == 0:
             agent.save_model(filename=model_name)
-
     env.close()
     if key:
         gym.upload(tdir, api_key=key)
@@ -36,20 +31,16 @@ def run(environment, model_name, key=None):
     tdir = tempfile.mkdtemp()
     env = gym.make(environment)
     env = gym.wrappers.Monitor(env, tdir, force=True)
-
     agent = DQNAgent(env, trained_model=model_name)
     EPISODES = 600
-
     for episode in range(EPISODES):
         state, reward, done = env.reset(), 0.0, False
         action = agent.action(state, reward, done, episode, training=False)
-
         while not done:
             # env.render()
             next_state, reward, done, _ = env.step(action)
             state = next_state
             action = agent.action(state, reward, done, episode)
-
     env.close()
     if key:
         gym.upload(tdir, api_key=key)
@@ -58,9 +49,8 @@ def run(environment, model_name, key=None):
 
 if __name__ == "__main__":
     environment = 'LunarLander-v2'
-    api_key = "sk_1u0Xao6NSfSBPBuplAbHvg"
+    api_key = None
     my_model = environment + '_model.h5'
 
     train(environment=environment, key=api_key, model_name=my_model)
-
     #run(environment=environment, key=api_key, model_name=my_model)
