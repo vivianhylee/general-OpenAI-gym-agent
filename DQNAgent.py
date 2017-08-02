546ae99773316075901161582292f33fa6e4ab5a
vivianhylee <vivianhylee@alumni.stanford.edu>
2017-08-01 19:30:40 -0700

    Add files via upload

---
+++ DQNAgent.py
@@ -0,0 +1,132 @@
+import h5py
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import load_model
+
+
+
+
+class DQNAgent:
+    def __init__(self, environment, trained_model=None):
+        # Initialize constant
+        self.environment = environment
+        self.state_size = environment.observation_space.shape[0]
+        self.action_size = environment.action_space.n
+        self.consecutive_episodes = 100
+
+        # Initialize neural network model and parameter
+        self.learning_rate = 0.0005
+        if trained_model:
+            self.model = self.load_model(filename=trained_model)
+        else:
+            self.model = self.build_model()
+
+        # Hyperparameters of the training
+        self.gamma = 0.99  # discount factor
+        self.replay_memory = 50000
+        self.replay_size = 128
+
+        # Exploration/exploitations parameters
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.05
+        self.episode_pure_exploration = 25
+
+        # Define variable
+        self.storage = deque(maxlen=self.replay_memory)
+        self.sum_reward, self.rewards_lst = 0.0, []
+
+
+    def build_model(self):
+        # Neural Network for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(200, input_dim=self.state_size, use_bias=True, activation='relu'))
+        model.add(Dense(200, use_bias=True, activation='relu'))
+        model.add(Dense(self.action_size, use_bias=True, activation='linear'))
+
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+
+    def store(self, state, action, reward, next_state, done):
+        # Save history to storage for replay
+        cnt_state = np.reshape(state, [1, self.state_size])
+        new_state = np.reshape(next_state, [1, self.state_size])
+        self.storage.append((cnt_state, np.array([action]), np.array([reward]), new_state, np.array([done])))
+
+    def action(self, state, reward, done, episode, training=True):
+        # Update cumulative reward
+        self.sum_reward += reward
+
+        # Episode ends
+        if done:
+            self.rewards_lst.append(self.sum_reward)
+            avg_reward = np.mean(self.rewards_lst[-self.consecutive_episodes: ])
+            print 'Episode %4d, Reward: %5d, Average rewards %5d' %(episode, self.sum_reward, avg_reward)
+            self.sum_reward = 0.0
+            self.epsilon = max(self.epsilon_decay * self.epsilon, self.epsilon_min)
+            return -1
+
+        # Return next action
+        else:
+            cnt_state = np.reshape(state, [1, self.state_size])
+            # Train agent
+            if training:
+                if episode >= self.episode_pure_exploration:
+                    self.replay()
+                    if np.random.random() < self.epsilon:
+                        action = self.environment.action_space.sample()
+                    else:
+                        act_values = self.model.predict(cnt_state)
+                        action = np.argmax(act_values[0])
+                else:
+                    action = self.environment.action_space.sample()
+
+            # Run trained agent
+            else:
+                act_values = self.model.predict(cnt_state)
+                action = np.argmax(act_values[0])
+
+            return action
+
+
+    def replay(self):
+        minibatch_idx = np.random.permutation(len(self.storage))[: self.replay_size]
+
+        states      = np.concatenate([self.storage[i][0] for i in minibatch_idx], axis=0)
+        actions     = np.concatenate([self.storage[i][1] for i in minibatch_idx], axis=0)
+        rewards     = np.concatenate([self.storage[i][2] for i in minibatch_idx], axis=0)
+        next_states = np.concatenate([self.storage[i][3] for i in minibatch_idx], axis=0)
+        dones       = np.concatenate([self.storage[i][4] for i in minibatch_idx], axis=0)
+
+        X_batch = np.copy(states)
+        Y_batch = np.zeros((self.replay_size, self.action_size), dtype=np.float64)
+
+        qValues_batch = self.model.predict(states)
+        qValuesNewState_batch = self.model.predict(next_states)
+
+        targetValue_batch = np.copy(rewards)
+        targetValue_batch += (1 - dones) * self.gamma * np.amax(qValuesNewState_batch, axis=1)
+
+        for idx in range(self.replay_size):
+            targetValue = targetValue_batch[idx]
+            Y_sample = qValues_batch[idx]
+            Y_sample[actions[idx]] = targetValue
+            Y_batch[idx] = Y_sample
+
+            if dones[idx]:
+                X_batch = np.append(X_batch, np.reshape(np.copy(next_states[idx]), (1, self.state_size)), axis=0)
+                Y_batch = np.append(Y_batch, np.array([[rewards[idx]] * self.action_size]), axis=0)
+
+        self.model.fit(X_batch, Y_batch, batch_size=len(X_batch), nb_epoch=1, verbose=0)
+
+
+    def save_model(self, filename):
+        self.model.save(filename)
+
+
+    def load_model(self, filename):
+        return load_model(filename)
---
+++ main.py
@@ -0,0 +1,66 @@
+import shutil
+import gym
+import tempfile
+from DQNAgent import DQNAgent
+
+
+def train(environment, model_name=None, key=None):
+    tdir = tempfile.mkdtemp()
+    env = gym.make(environment)
+    env = gym.wrappers.Monitor(env, tdir, force=True)
+
+    agent = DQNAgent(env)
+    EPISODES = 600
+
+    for episode in range(EPISODES):
+        state, reward, done = env.reset(), 0.0, False
+        action = agent.action(state, reward, done, episode)
+
+        while not done:
+            # env.render()
+            next_state, reward, done, _ = env.step(action)
+            agent.store(state, action, reward, next_state, done)
+            state = next_state
+            action = agent.action(state, reward, done, episode)
+
+        if model_name and episode % 50 == 0:
+            agent.save_model(filename=model_name)
+
+    env.close()
+    if key:
+        gym.upload(tdir, api_key=key)
+    shutil.rmtree(tdir)
+
+
+def run(environment, model_name, key=None):
+    tdir = tempfile.mkdtemp()
+    env = gym.make(environment)
+    env = gym.wrappers.Monitor(env, tdir, force=True)
+
+    agent = DQNAgent(env, trained_model=model_name)
+    EPISODES = 600
+
+    for episode in range(EPISODES):
+        state, reward, done = env.reset(), 0.0, False
+        action = agent.action(state, reward, done, episode, training=False)
+
+        while not done:
+            # env.render()
+            next_state, reward, done, _ = env.step(action)
+            state = next_state
+            action = agent.action(state, reward, done, episode)
+
+    env.close()
+    if key:
+        gym.upload(tdir, api_key=key)
+    shutil.rmtree(tdir)
+
+
+if __name__ == "__main__":
+    environment = 'LunarLander-v2'
+    api_key = "sk_1u0Xao6NSfSBPBuplAbHvg"
+    my_model = environment + '_model.h5'
+
+    train(environment=environment, key=api_key, model_name=my_model)
+
+    #run(environment=environment, key=api_key, model_name=my_model)
